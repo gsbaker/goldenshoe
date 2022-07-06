f1040710a063a61d0219facda8ab4a755eb8a27e
gsbaker <gsbaker2000@icloud.com>
2022-07-06 12:31:21 +0100

    Update Product, add ProductImage, and update admin classes

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from shop.models import Product, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline, ]
+
+
+admin.site.register(Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -4,5 +4,21 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=30)
-    price = models.IntegerField()
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    name = models.CharField(max_length=200, default='')
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M')
+    color = models.CharField(max_length=200, default='')
+    brand = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.name + ' ({brand})'.format(brand=self.brand)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='shoes')
+
